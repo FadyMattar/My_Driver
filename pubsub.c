cca4271499e03ce86bbf2bfc2d56ea30ea1ecd47
Fady Mattar <149119364+FadyMattar@users.noreply.github.com>
2025-01-27 12:53:48 +0200

    code fix

--- pubsub.c
+++ pubsub.c
@@ -27,7 +27,6 @@ MODULE_LICENSE("GPL");
 
 /* globals */
 int my_major = 0; /* will hold the major # of my device driver */
-
 struct file_operations my_fops = {
     .open = my_open,
     .release = my_release,
@@ -41,22 +40,19 @@ struct pdp_strct {
     int minor_id;
     unsigned long type;
     int seek;
-    int is_writer;
-    struct list_head list;
+    int my_resets;
 };
 
 struct buffer_struct {
-    int publisher_exists;
     int sub_counter;
     int finished_sub;
     int buff_len;
     char *buff;
-    struct pdp_strct *writer_pdp;
-    struct list_head *ins_pdp;
+    int global_reset;
     int reference_count;
 };
 
-struct buffer_struct *buffer_array[MINOR_NUM];
+struct buffer_struct *buffer_array;//[MINOR_NUM];
 
 int init_module(void)
 {
@@ -72,16 +68,14 @@ int init_module(void)
 
     // we initialize the buffer array pointers:
     for ( int i=0 ; i < MINOR_NUM ; i++) {
-        buffer_array[i] = malloc(sizeof(buffer_struct));
+        buffer_array[i] = kmalloc(sizeof(buffer_struct));
         if (buff_p == NULL) { return -ENOMEM;}
-        buffer_array[i]->publisher_exists = 0;
-        buffer_array[i]->sub_counter = 0;
-        buffer_array[i]->finished_sub
-        buffer_array[i]->buff_len = 0;
-        buffer_array[i]->buff = NULL;
-        buffer_array[i]->writer_pdp = NULL;
-        INIT_LIST_HEAD(&buffer_array[i]->ins_pdp);
-        buffer_array[i]->reference_count = 0;
+        buffer_array[i].sub_counter = 0;
+        buffer_array[i].finished_sub
+        buffer_array[i].buff_len = 0;
+        buffer_array[i].buff = NULL;
+        buffer_array[i].global_reset = 0;
+        buffer_array[i].reference_count = 0;
     }
 
     return 0;
@@ -95,7 +89,7 @@ void cleanup_module(void)
     unregister_chrdev(my_major, MY_DEVICE);
 
     for ( int i=0 ; i < MINOR_NUM ; i++) {
-        free(buffer_array[i]);
+        kfree(buffer_array[i]);
     }
     return;
 }
@@ -104,48 +98,47 @@ void cleanup_module(void)
 int my_open(struct inode *inode, struct file *filp)
 {
     //init pdp pointer and put in filp private_data:
-    struct pdp_strct *p = malloc ( sizeof (struct pdp_struct));
-    if (buff_p == NULL) { return -ENOMEM;}
+    struct pdp_strct *p = kmalloc ( sizeof (struct pdp_struct));
+    if (p == NULL) { return -ENOMEM;}
+
     p->minor_id = MINOR(inode->i_rdev);
     p->type = TYPE_NONE;
     p->seek = 0 ; 
-    filp->private_data = p;
-  
+    p->my_resets = buffer_array[p->minor_id].global_reset;
+    filp->private_data = *p; // might be &p
+
+    // check if buffer is initiated, if not then initiate
     if (buffer_array[p->minor_id]->buff == NULL) {
-        char *buff_p = malloc ( sizeof(char)*BUFFER_SIZE);
-        if (buff_p == NULL) { return -ENOMEM;}
-        buffer_array[p->minor_id]->buff = buff_p;
+        char *buff_p = kmalloc ( sizeof(char)*BUFFER_SIZE);
+        if (*buff_p == NULL) { return -ENOMEM;}
+        buffer_array[p->minor_id].buff = *buff_p;
     }
-
-    // Check if there is a writer 
-    if ( (filp -> f_mode == FMODE_WRITE) && (buffer_array[p->minor_id].publisher_exists) ) {
-        -EFAULT;
-    }
-
-    list_add( &p, &buffer_array[p->minor_id]->ins_pdp);
     
     return 0;
 }
 
-int my_release(struct inode *inode, struct file *filp)
+int my_release(struct inode *inode, struct file *filp) // release memory initiated in open
 {
     struct pdp_strct *pdp_p = (struct pdp_strct)filp->private_data; 
     int minor = pdp_p->minor_id;
     
-    free(&pdp_p);
+    if (pdp_p->type == TYPE_SUB) {
+        buffer_array[minor].sub_counter --;
+    }
+
+    kfree(&pdp_p);
     
-    buffer_array[minor]->reference_count -=1;
+    buffer_array[minor].reference_count -=1;
+
 
     if( reference count == 0 ) {
-        free(buffer_array[minor]->buff);
-        buffer_array[minor]->publisher_exists = 0;
-        buffer_array[minor]->sub_counter = 0;
-        buffer_array[minor]->finished_sub
-        buffer_array[minor]->buff_len = 0;
-        buffer_array[minor]->buff = NULL;
-        buffer_array[minor]->writer_pdp = NULL;
-        INIT_LIST_HEAD(&buffer_array[minor]->ins_pdp);
-        buffer_array[minor]->reference_count = 0;
+        kfree(buffer_array[minor].buff);
+        buffer_array[minor].sub_counter = 0;
+        buffer_array[minor].finished_sub
+        buffer_array[minor].buff_len = 0;
+        buffer_array[minor].buff = NULL;
+        buffer_array[minor].global_reset = 0;
+        buffer_array[minor].reference_count = 0;
     }
     
     return 0;
@@ -156,19 +149,27 @@ ssize_t my_read(struct file *filp, char *buf, size_t count, loff_t *f_pos)
     //find minor
     struct pdp_strct *pdp_p = (struct pdp_strct)filp->private_data; 
     int minor = pdp_p->minor_id;
-    int *seek = &pdp_p->seek;
+
     //check type
     if (pdp_p->type != TYPE_SUB) {
         return -EPERM;
     }
     
     //check if the buffer of the file exists
-    if (buffer_array[minor]->buff == NULL) {
+    if (buffer_array[minor].buff == NULL) {
         return -EFAULT;
     }
 
+    //check if we have to reset
+    if (buffer_array[minor].global_reset != pdp_p->my_resets) {
+        pdp_p->seek = 0;
+        pdp_p->my_resets = buffer_array[minor].global_reset;
+    }
+
+    int *seek = &pdp_p->seek;
+
     // find how much to read
-    int read_count = buffer_array[minor]->buff_len - *seek;
+    int read_count = buffer_array[minor].buff_len - *seek;
     if (read_count == 0) {
         return -EAGAIN;
     }
@@ -177,24 +178,20 @@ ssize_t my_read(struct file *filp, char *buf, size_t count, loff_t *f_pos)
     }
 
     // copy to the reader buffer
-    if (copy_to_user ( buf, buffer_array[minor]->buff, read_count)) {
+    if (copy_to_user ( buf, buffer_array[minor].buff, read_count)) {
         return -EBADF;
     }    
 
     // update seek according to the amount read.
     *seek += read_count;
-    if ( *seek == buffer_array[minor]->buff_len) {
-        buffer_array[minor]->finished_sub += 1;
+    if ( *seek == buffer_array[minor].buff_len) {
+        buffer_array[minor].finished_sub += 1;
     }
     // check if all subs are done reading
-    if( buffer_array[minor]->finished_sub ==  buffer_array[minor]->sub_counter) {
-        buffer_array[minor]->writer_pdp->is_writer = 0;
-        buffer_array[minor]->publisher_exists = 0;
-        buffer_array[minor]->writer_pdp = NULL;
-        struct pdp_strct *entry;
-        list_for_each_entry( entry, &buffer_array[minor]->ins_pdp , list) {
-            entry->seek = 0 ; 
-        }
+    if( buffer_array[minor].finished_sub ==  buffer_array[minor].sub_counter) {
+        buffer_array[minor].global_reset += 1;
+        buffer_array[minor].buff_len = 0;
+        buffer_array[minor].finished_sub = 0;
     }
 
     return read_count; 
@@ -216,35 +213,21 @@ ssize_t my_write(struct file *filp, const char *buf, size_t count, loff_t *f_pos
     }
 
     //check remaining space
-    int remaining_buffer_spcae = BUFFER_SIZE - buffer_array[minor]->buff_len;
+    int remaining_buffer_spcae = BUFFER_SIZE - buffer_array[minor].buff_len;
     if (count > remaining_buffer_spcae ) {
         return -EAGAIN;
     }
 
     //check if the buffer of the file exists
-    if (buffer_array[minor]->buff == NULL) {
+    if (buffer_array[minor].buff == NULL) {
         return -EFAULT;
-    }
-
-    // check if i am the only writer 
-    if (buffer_array[minor]->publisher_exists && !pdp_p->is_writer) {
-        return -EFAULT;
-    }
-
-    //update 
-    if ( !buffer_array[minor]->publisher_exists ) {
-        buffer_array[minor]->publisher_exists = 1; 
-        pdp_p->is_writer = 1;
-        buffer_array[minor]->writer_pdp = pdp_p;
-        buffer_array[minor]->buff_len = 0;
-        f_pos = 0;
     }
 
     //copy from user to our buffer
-    if ( copy_from_user(buffer_array[minor]->buff + buffer_array[minor]->buff_len,buf,count) ) {
+    if ( copy_from_user(buffer_array[minor].buff + buffer_array[minor].buff_len,buf,count) ) {
         return -EBADF;
     }
-    buffer_array[minor]->buff_len += count;
+    buffer_array[minor].buff_len += count;
 
     return count;
 }
@@ -254,6 +237,7 @@ ssize_t my_write(struct file *filp, const char *buf, size_t count, loff_t *f_pos
 int my_ioctl(struct inode *inode, struct file *filp, unsigned int cmd, unsigned long arg)
 {
     struct pdp_strct *pdp_p = (struct pdp_strct) filp->private_data;
+    int minor = pdp_p->minor_id;
     switch(cmd)
     {
     case SET_TYPE:
@@ -264,6 +248,9 @@ int my_ioctl(struct inode *inode, struct file *filp, unsigned int cmd, unsigned 
             return -EPERM;
         }
         pdp_p->type = arg;
+        if (pdp_p->type == TYPE_SUB) {
+            buffer_array[minor].sub_counter ++;
+        }
         return 0;
 	break;
     case GET_TYPE:
